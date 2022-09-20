b907d56927418b920cb4d7987f23cfaad5d232bd
vVTRr <101228066+vVTRr@users.noreply.github.com>
2022-09-20 19:39:23 -0300

    Create Número é primo.js

---
+++ Número é primo.js
@@ -0,0 +1,24 @@
+
+for (let i = 0; i < 100 ; i++){ 
+
+    var N = parseInt(lines.shift()); 
+
+    if( N/2 && N/3 && N/5 && N/7 && N/11 && N/13 && N/17 && N/19 && N/23 && N/29 && N/31 && N/37 && N/41 == 0 ) 
+
+{ 
+
+        console.log( N + " nao e primo" ); 
+
+    } else { 
+
+        console.log( N + " e primo" ); 
+
+    } 
+
+Somatório: Soma de um conjunto de números. 
+
+var A = parseInt(lines.shift()); 
+
+var B = parseInt(lines.shift()); 
+
+  
